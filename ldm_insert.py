e303aa117c056ab95ebeadb51e6fb230e80e9beb
Eric Bruning <eric.bruning@gmail.com>
2018-10-28 23:50:26 +0000

    Allow sector part of filename to vary

--- ldm_insert.py
+++ ldm_insert.py
@@ -21,7 +21,7 @@ def pqinsert(filename, container='ldm-prod'):
     output = subprocess.check_output(pqcommand, stderr=subprocess.STDOUT)
     # return output
 
-def await_grid(grid_dir, action):
+def await_grid(grid_dir, scene_id, action):
     # Fetch the previous minute of data. Get one minute ago, and then round down
     # to the nearest minute
     dt1min = timedelta(0, 60)
@@ -39,9 +39,9 @@ def await_grid(grid_dir, action):
     grid_path = os.path.join(grid_dir, startdate.strftime('%Y/%b/%d'))
     # "OR_GLM-L2-GLMC-M3_G16_s20181011100000_e20181011101000_c20181011124580.nc 
     # Won't know file created time.
-    dataset_name = "OR_GLM-L2-GLMC-{0}_{1}_s{2}_e{3}_c*.nc".format(
+    dataset_name = "OR_GLM-L2-GLM{4}-{0}_{1}_s{2}_e{3}_c*.nc".format(
         mode, platform, startdate.strftime('%Y%j%H%M%S0'),
-        enddate.strftime('%Y%j%H%M%S0'))
+        enddate.strftime('%Y%j%H%M%S0'), scene_id)
     expected_file = os.path.join(grid_path, dataset_name)
 
     while True:
@@ -71,9 +71,13 @@ def create_parser():
         required=True, dest='grid_dir', action='store',
         help="Gridded data will be saved to this directory, in subdirectories" 
              "like /2018/Jul/04/")
+    parser.add_argument('-c', '--scene', dest='scene', action='store',
+        default='C',
+        help="One of C, M1, M2, or F, matching the scene ID part of the"
+             " filename")
     return parser
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    print(await_grid(args.grid_dir, pqinsert))
+    print(await_grid(args.grid_dir, args.scene, pqinsert))
